f6462ede7f116c0e58e9b2f8a938e185e6091d75
hermanwikner <wiknerherman@hotmail.com>
2016-10-14 17:30:30 +0200

    Uploaded our code

---
+++ bigscreentest/public/script.js
@@ -0,0 +1,21 @@
+var counter = 0;
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    console.log("RUNK");
+    move();
+});
+
+
+function move(){
+
+$('#object').animate({top: '10%'});
+$('#object').animate({top: '50%'});
+$('#object').animate({left: '10%'});
+$('#object').animate({left: '70%', top: '80%'});
+$('#object').animate({top: '50%'});
+$('#object').animate({left: '50%'}, move);
+counter++;
+console.log("counter: " + counter);
+
+}
